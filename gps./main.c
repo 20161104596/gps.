15a48e93eecc1961967a4916140e60d1f8bb03ce
20161104596 <964312086@qq.com>
2017-06-22 11:35:11 +0800

    提取信息
    提取信息

--- gps./main.c
+++ gps./main.c
@@ -11,12 +11,38 @@
 int main()
 {
     FILE *fp1;
-    fp1=fopen("//Users//a20161104596//Desktop//gps.//input.txt","r");
+    fp1=fopen("//Users//a20161104596//Desktop//gps.//GPS170510.log","r+");
+    char str1[70];
+    char str2[70];
+    char lat[9];
+    char time[6];
     int i;
-    char s[10];
-    for(i=1;i<=10;i++)
-    {
-        sprintf(fp1,"%d",&s[1]);
-    }
-    
+    fscanf(fp1,"%s%s",str1,str2);
+    printf("结果：%s\n%s\n",str1,str2);
+    for(i=0;i<8;i++)
+        lat[i]=str1[i+16];
+    lat[8]='\0';
+    printf("纬度：%s\n",lat);
+    for(i=0;i<6;i++)
+        lat[i]=str1[i+7];
+    lat[6]='\0';
+    printf("时间：%s\n",lat);
+    for(i=0;i<9;i++)
+        lat[i]=str1[i+27];
+    lat[9]='\0';
+    printf("经度：%s\n",lat);
+    for(i=0;i<5;i++)
+        lat[i]=str1[i+39];
+    lat[5]='\0';
+    printf("地面速率：%s\n",lat);
+    for(i=0;i<5;i++)
+        lat[i]=str1[i+45];
+    lat[5]='\0';
+    printf("地面航向：%s\n",lat);
+    for(i=0;i<6;i++)
+        lat[i]=str1[i+51];
+    lat[6]='\0';
+    printf("日期：%s\n",lat);
+    fclose(fp1);
+    return 0;
 }
